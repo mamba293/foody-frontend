9282d135b976451ad30a259b70a08a1d94c3bd6f
Mamba <haleckijigor13@gmail.com>
2025-10-19 16:21:10 +0300

    add db config and authenticate

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import app from './app.js';
+import { initModels } from './models/init-models.js';
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try {
+        await initModels();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to start the server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
