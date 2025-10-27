e89248730f0a7f15633077580d98484107e0590a
Mamba <haleckijigor13@gmail.com>
2025-10-27 14:43:30 +0300

    add navgation on dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 import AuthPage from "./pages/AuthPage/AuthPage.jsx";
 import Dashboard from "./pages/DashboardPage/DashboardPage";
+import BusinessPage from "./pages/BusinessPage/BusinessPage.jsx";
+import ClientPage from "./pages/ClientPage/ClientPage.jsx";
 
 import "./App.css";
 
@@ -11,6 +13,8 @@ function App() {
    <Route path="/" element={<LandingPage />} />
    <Route path="/auth" element={<AuthPage />} />
    <Route path="/dashboard" element={<Dashboard />} />
+   <Route path="/business" element={<BusinessPage />} />
+   <Route path="/client" element={<ClientPage />} />
   </Routes>
  );
 }
--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,30 +1,29 @@
-import { useNavigate } from "react-router-dom";
+    import { useNavigate } from "react-router-dom";
 
-import styles from "./DashboardPage.module.css";
+    import styles from "./DashboardPage.module.css";
 
-export default function DashboardPage() {
- const nav = useNavigate();
+    export default function DashboardPage() {
+    const nav = useNavigate();
 
- const onClientClick = () => {};
- const onBusinessClick = () => {
-    
- };
+    const onClientClick = () => nav("/client");
 
- return (
-  <div className={styles.dashboard}>
-   <section>
-    <h1>Войти как</h1>
-   </section>
-   <div className={styles.buttonsContainer}>
-    <button className={styles.button} onClick={onClientClick}>
-     <p>Клиент</p>
-     <img src="src/public/png/client.png" alt="Клиент" />
-    </button>
-    <button className={styles.button} onClick={onBusinessClick}>
-     <p>Бизнес</p>
-     <img src="src/public/png/business.png" alt="Бизнес" />
-    </button>
-   </div>
-  </div>
- );
-}
+    const onBusinessClick = () => nav("/business");
+
+    return (
+    <div className={styles.dashboard}>
+    <section>
+        <h1>Войти как</h1>
+    </section>
+    <div className={styles.buttonsContainer}>
+        <button className={styles.button} onClick={onClientClick}>
+        <p>Клиент</p>
+        <img src="src/public/png/client.png" alt="Клиент" />
+        </button>
+        <button className={styles.button} onClick={onBusinessClick}>
+        <p>Бизнес</p>
+        <img src="src/public/png/business.png" alt="Бизнес" />
+        </button>
+    </div>
+    </div>
+    );
+    }
